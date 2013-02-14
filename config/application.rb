ae3c979409e23b8076b9b12c764b351ba048aec0
Albert Viscasillas <aviscasillas@gmail.com>
2013-02-14 09:29:15 +0100

    Fixing javascript not work on heroku

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  Bundler.require(:default, :assets, Rails.env)
+  #Bundler.require(:default, :assets, Rails.env)
+  # For assets precompiling on heroku
+  Bundler.require *Rails.groups(:assets)
 end
 
 module Corruptedia
